c4f34bfdb521a9131c70c94c0f4475a3138c1f81
JennyferValeskaVera18 <valevarillas13@gmail.com>
2023-12-11 20:58:19 -0500

    Hola

---
+++ modulos.js
@@ -0,0 +1,22 @@
+//CONTADOR DEL 1 AL 10
+function contar () {
+    for (let i=1; i<=10; i++){
+        console.log(i);
+    }
+}
+
+//FUNCION PARA SALUDAR CON PARAMETRO
+function saludar(nombre) {
+    console.log(`HI ${nombre}, you have to practice more your english.`)
+}
+//PI
+const PI = 3.14159;
+
+//Exportamos la función
+export {contar, saludar, PI};
+
+
+
+
+
+
---
+++ s29.js
@@ -0,0 +1,8 @@
+//IMPORTAMOS LO QUE QUEREMOS
+import {contar, saludar, PI} from "./modulos.js" ;
+
+
+//Llamar la función
+contar();
+saludar("Valeska");
+console.log(`El valor de PI es: ${PI}`)
